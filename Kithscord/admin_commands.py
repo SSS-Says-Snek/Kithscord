6af7f3388716a30bf79848e29d5e6e3f5f9b9f08
Ankith <46915066+ankith26@users.noreply.github.com>
2021-04-14 21:21:09 +0530

    Use OOP to handle commands

--- Kithscord/admin_commands.py
+++ Kithscord/admin_commands.py
@@ -1,9 +1,37 @@
+import sys
+
 import discord
 
-from overload import *
 import util
+from user_commands import UserCommand
 
 
-@export(-1, True)
-async def error(args, msg: discord.Message, response: discord.Message):
-    raise Exception("bruh", "bruh")
+class AdminCommand(UserCommand):
+    """
+    Base class to handle admin commands.
+    """
+    async def cmd_error(self):
+        """
+        Temporary
+        """
+        raise Exception("bruh", "bruh")
+
+    async def cmd_sudo(self):
+        """
+        Implement kh!sudo, for admins to send messages via the bot
+        """
+        await self.invoke_msg.channel.send(self.string)
+        await self.response.delete()
+        await self.invoke_msg.delete()
+
+    async def cmd_stop(self):
+        """
+        Implement kh!stop, for admins to stop the bot
+        """
+        self.check_args(0)
+        await util.edit_embed(
+            self.response,
+            "Stopping bot...",
+            "I gotta go now, but I will BRB"
+        )
+        sys.exit(0)
